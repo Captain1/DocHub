51aefc3dfdf1340fee519e3cb2132ad5fd4147fa
Frank Wang <frankwdy11@gmail.com>
2015-04-11 00:50:30 +0100

    doc serializer add :created_at

--- app/serializers/doc_serializer.rb
+++ app/serializers/doc_serializer.rb
@@ -1,4 +1,4 @@
 class DocSerializer < ActiveModel::Serializer
-  attributes :id, :user_id,:title, :topic, :license, :description, :filepicker_url
+  attributes :id, :user_id,:title, :topic, :license, :description, :filepicker_url, :created_at
   has_many :doc_comments
 end
