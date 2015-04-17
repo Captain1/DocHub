226be14de12ea84892f0ec70d65518ee18c26bfe
Frank Wang <frankwdy11@gmail.com>
2015-04-17 19:15:19 +0100

    adjust seq2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery-2.1.3.min
-//= require jquery_ujs
 //= require bootstrap.min
-//= require turbolinks
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
